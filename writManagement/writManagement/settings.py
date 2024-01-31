5eecf2033a2cecb9718700530a728033e06c36f8
hemangkhurana <hemangkhurana@gmail.com>
2024-02-01 02:37:32 +0530

    added google cloud postgre database connection details in settings.py

--- writManagement/writManagement/settings.py
+++ writManagement/writManagement/settings.py
@@ -83,13 +83,24 @@ WSGI_APPLICATION = 'writManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dssDB',
+# 	'USER':'postgres',
+# 	'PASSWORD':'7078',
+# 	"HOST":'127.0.0.1',
+# 	"PORT":'5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dssDB',
+        'NAME': 'writDBpostgre',
 	'USER':'postgres',
-	'PASSWORD':'7078',
-	"HOST":'127.0.0.1',
+	'PASSWORD':'RajatGoyal',
+	"HOST":'34.28.126.107',
 	"PORT":'5432',
     }
 }
