54f692494e1b4cbfea36e828adb68eb236ca12c8
hemangkhurana <hemangkhurana@gmail.com>
2024-04-26 22:10:18 +0530

    added CRUD functionality for departments in schedule module.

--- writManagement/schedule/urls.py
+++ writManagement/schedule/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import  views 
 urlpatterns = [
-    path('get_all_departments',views.get_all_departments),
     path('get_meetings',views.get_meetings),
     path('add_department', views.add_department),
     path('hemang', views.hemang),
     path('get_departments', views.get_departments),
+    path('delete_department', views.delete_department),
+    path('update_department', views.update_department),
 ]
--- writManagement/schedule/views.py
+++ writManagement/schedule/views.py
@@ -3,59 +3,50 @@ from django.views.decorators.http import require_http_methods
 from datetime import datetime
 import json
 from .models import departments
-
-
-@require_http_methods(["GET"])
-def get_all_departments(request):
-    departments = [
-        {'departmentId': 1, 'departmentName': 'Department A', 'departmentDescription': 'Description for Department A', 'departmentUsers': ['Hemang', 'Deepanshu']},
-        {'departmentId': 2, 'departmentName': 'Department B', 'departmentDescription': 'Description for Department B', 'departmentUsers': ['Hemang', 'Rajat']},
-        {'departmentId': 3, 'departmentName': 'Department C', 'departmentDescription': 'Description for Department C', 'departmentUsers': ['Rajat', 'Ankush']},
-    ]
-    return JsonResponse({'success': True, 'data': departments})
+from bson import ObjectId
 
 
 @require_http_methods(["GET"])
 def get_meetings(request):
     events = [
         {
-          'id': 1,
-          'title': 'Meeting 111',
-          'start': datetime(2024, 3, 10, 4, 0), 
-          'end': datetime(2024, 3, 10, 6, 0),   
-          'location': 'DC Office',
-          'priority': 'high',
-          'departments': ['Marketing', 'Development', 'HR'],
-          'groups': ['Group 1', 'Group 2'],
-          'users': ['User 1', 'User 2'],
-          'minutesOfMeeting': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          'summary': 'Meeting went well. Discussed project milestones and assigned tasks.',
+            'id': 1,
+            'title': 'Meeting 111',
+            'start': datetime(2024, 3, 10, 4, 0), 
+            'end': datetime(2024, 3, 10, 6, 0),   
+            'location': 'DC Office',
+            'priority': 'high',
+            'departments': ['Marketing', 'Development', 'HR'],
+            'groups': ['Group 1', 'Group 2'],
+            'users': ['User 1', 'User 2'],
+            'minutesOfMeeting': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'summary': 'Meeting went well. Discussed project milestones and assigned tasks.',
         },
         {
-          'id': 3,
-          'title': 'Meeting 3',
-          'start': datetime(2024, 3, 10, 10, 0), 
-          'end': datetime(2024, 3, 10, 12, 0),   
-          'location': 'DC Office',
-          'priority': 'medium',
-          'departments': ['Marketing', 'Development', 'HR'],
-          'groups': ['Group 3'],
-          'users': ['User 1', 'User 2'],
-          'minutesOfMeeting': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          'summary': 'Meeting went well. Discussed project milestones and assigned tasks.',
+            'id': 3,
+            'title': 'Meeting 3',
+            'start': datetime(2024, 3, 10, 10, 0), 
+            'end': datetime(2024, 3, 10, 12, 0),   
+            'location': 'DC Office',
+            'priority': 'medium',
+            'departments': ['Marketing', 'Development', 'HR'],
+            'groups': ['Group 3'],
+            'users': ['User 1', 'User 2'],
+            'minutesOfMeeting': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'summary': 'Meeting went well. Discussed project milestones and assigned tasks.',
         },
         {
-          'id': 2,
-          'title': 'Meeting 2',
-          'start': datetime(2024, 3, 25, 14, 0), 
-          'end': datetime(2024, 3, 25, 16, 0),    
-          'location': 'DC Office',
-          'priority': 'low',
-          'departments': ['Marketing', 'Development', 'HR'],
-          'groups': ['Group 2'],
-          'users': ['User 1', 'User 2'],
-          'minutesOfMeeting': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          'summary': 'Meeting went well. Discussed project milestones and assigned tasks.',
+            'id': 2,
+            'title': 'Meeting 2',
+            'start': datetime(2024, 3, 25, 14, 0), 
+            'end': datetime(2024, 3, 25, 16, 0),    
+            'location': 'DC Office',
+            'priority': 'low',
+            'departments': ['Marketing', 'Development', 'HR'],
+            'groups': ['Group 2'],
+            'users': ['User 1', 'User 2'],
+            'minutesOfMeeting': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'summary': 'Meeting went well. Discussed project milestones and assigned tasks.',
         },
     ]
     return JsonResponse({'success': True, 'data': events})
@@ -77,11 +68,55 @@ def add_department(request):
     except Exception as e:
         return JsonResponse({'success': False, 'error': e})
     
-@require_http_methods({"GET"})
+
+@require_http_methods(["GET"])
 def get_departments(request):
     try:
         all_departments = departments.find({})
-        print(all_departments)
-        return JsonResponse({'success': True, 'data' : all_departments})
+        data = []
+        for x in all_departments:
+            temp = {}
+            temp['_id'] = str(x['_id'])
+            temp['departmentName'] = x['departmentName']
+            temp['departmentDescription'] = x['departmentDescription']
+            data.append(temp)
+        print(data)
+        return JsonResponse({'success': True, 'data' : data})
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': e})
+    
+@require_http_methods(["POST"])
+def delete_department(request):
+    try:
+        object_id = json.loads(request.body)
+        print(object_id)
+        if object_id:
+            result = departments.delete_one({'_id': ObjectId(object_id)})
+            if result.deleted_count == 1:
+                return JsonResponse({'success': True, 'message': 'Department Deleted Successfully'})
+            else:
+                return JsonResponse({'success': False, 'message': 'Department not found or already deleted'})
+        else:
+            return JsonResponse({'success': False, 'message': 'Object ID not provided'})
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': e})
+
+@require_http_methods(["POST"])
+def update_department(request):
+    try:
+        data = json.loads(request.body)
+        department_id = data.get('_id')
+        print(data)
+        if department_id:
+            result = departments.update_one(
+                {'_id': ObjectId(department_id)},
+                {'$set': {'departmentName': data['departmentName'], 'departmentDescription': data['departmentDescription']}}
+            )
+            if result.modified_count == 1:
+                return JsonResponse({'success': True, 'message': 'Department Updated Successfully'})
+            else:
+                return JsonResponse({'success': False, 'message': 'Department not found or no changes made'})
+        else:
+            return JsonResponse({'success': False, 'message': 'Object ID not provided'})
     except Exception as e:
         return JsonResponse({'success': False, 'error': e})
